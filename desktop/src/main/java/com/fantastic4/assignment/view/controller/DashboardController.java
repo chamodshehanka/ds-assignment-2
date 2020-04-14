76e51d943c241058de326b4df51744f1cc4b5552
Chamod Shehanka <hcsperera@outlook.com>
2020-04-15 01:55:48 +0530

    rest dependecies added

---
+++ desktop/src/main/java/com/fantastic4/assignment/view/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package com.fantastic4.assignment.view.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
