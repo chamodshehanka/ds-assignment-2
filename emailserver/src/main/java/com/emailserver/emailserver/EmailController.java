7e4ad020713ffd3569dc2a5227d08b778507a5bf
Randud <randu612@gmail.com>
2020-04-30 02:12:25 +0530

    Email server successfully created

---
+++ emailserver/src/main/java/com/emailserver/emailserver/EmailController.java
@@ -0,0 +1,21 @@
+package com.emailserver.emailserver;
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+	
+	@Autowired
+	private EmailAlert emailAlert;
+
+	@RequestMapping("/send-mail")
+	public void sendMail() throws MessagingException {
+		
+		emailAlert.send("afirealert@gmail.com", "Fire Alert !", "Test");
+		
+	}
+
+}
