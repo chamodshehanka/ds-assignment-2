c8b658f2ed458343d6302be30cd49a8cfd77978d
Randud <randu612@gmail.com>
2020-04-30 15:50:02 +0530

    SMS server twilio server page added

--- SMS-Server/src/main/java/com/sms/server/SMSServer/SmsServerApplication.java
+++ SMS-Server/src/main/java/com/sms/server/SMSServer/SmsServerApplication.java
@@ -1,13 +1,25 @@
 package com.sms.server.SMSServer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.twilio.Twilio;
+
 @SpringBootApplication
-public class SmsServerApplication {
+public class SmsServerApplication implements CommandLineRunner{
+	
+	private final static String ACCOUNT_SID ="AC591f0ae975834806acebdeee5239c449";
+	private final static String AUTH_TOKEN = "29cad427d6840ee62626fa0b437ddfa1";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmsServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Twilio.init(ACCOUNT_SID,AUTH_TOKEN);
 	}
 
 }
