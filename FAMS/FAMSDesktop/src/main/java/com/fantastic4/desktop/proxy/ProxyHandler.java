5303813c98070774fc21041d2511edb414fbe13f
Chamod Shehanka <hcsperera@outlook.com>
2020-05-02 17:02:39 +0530

    Proxy making

--- FAMS/FAMSDesktop/src/main/java/com/fantastic4/desktop/proxy/ProxyHandler.java
+++ FAMS/FAMSDesktop/src/main/java/com/fantastic4/desktop/proxy/ProxyHandler.java
@@ -1,4 +1,29 @@
 package com.fantastic4.desktop.proxy;
 
-public class ProxyHandler {
+import com.fantastic4.common.services.ServicesFactory;
+import com.fantastic4.common.services.SuperService;
+import com.fantastic4.common.services.custom.SensorService;
+
+public class ProxyHandler implements ServicesFactory {
+
+    private static ProxyHandler proxyHandler;
+    private SensorService sensorService;
+
+    private ProxyHandler() {
+        
+    }
+
+    public static ProxyHandler getInstance(){
+        if (proxyHandler == null)
+            proxyHandler = new ProxyHandler();
+        return proxyHandler;
+    }
+
+    @Override
+    public SuperService getService(ServicesType servicesType) throws Exception {
+        switch (servicesType) {
+            case SENSOR: return sensorService;
+            default: return null;
+        }
+    }
 }
