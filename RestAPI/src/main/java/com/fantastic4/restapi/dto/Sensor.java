9e0f3f4bf41338b22c11ae536bac76236dd83bcc
Chamod Shehanka <hcsperera@outlook.com>
2020-05-04 03:26:11 +0530

    Rest API update and Web update

--- RestAPI/src/main/java/com/fantastic4/restapi/dto/Sensor.java
+++ RestAPI/src/main/java/com/fantastic4/restapi/dto/Sensor.java
@@ -1,14 +1,22 @@
 package com.fantastic4.restapi.dto;
 
+import java.util.List;
+
 public class Sensor {
 
     private String sensorID;
+    private int floorNo;
+    private int roomNo;
+    private List<SensorData> sensorData;
 
     public Sensor() {
     }
 
-    public Sensor(String sensorID) {
+    public Sensor(String sensorID, int floorNo, int roomNo, List<SensorData> sensorData) {
         this.sensorID = sensorID;
+        this.floorNo = floorNo;
+        this.roomNo = roomNo;
+        this.sensorData = sensorData;
     }
 
     public String getSensorID() {
@@ -17,5 +25,29 @@ public class Sensor {
 
     public void setSensorID(String sensorID) {
         this.sensorID = sensorID;
+    }
+
+    public int getFloorNo() {
+        return floorNo;
+    }
+
+    public void setFloorNo(int floorNo) {
+        this.floorNo = floorNo;
+    }
+
+    public int getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(int roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public List<SensorData> getSensorData() {
+        return sensorData;
+    }
+
+    public void setSensorData(List<SensorData> sensorData) {
+        this.sensorData = sensorData;
     }
 }
