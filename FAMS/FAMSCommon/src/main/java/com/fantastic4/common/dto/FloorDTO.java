f1d7d697037f164a4754647f92c31a7391fb2a93
deSamds <samanthi20258@gmail.com>
2020-05-06 14:55:59 +0530

    FloorDTO added and Repo Impl updated

--- FAMS/FAMSCommon/src/main/java/com/fantastic4/common/dto/FloorDTO.java
+++ FAMS/FAMSCommon/src/main/java/com/fantastic4/common/dto/FloorDTO.java
@@ -1,4 +1,37 @@
 package com.fantastic4.common.dto;
 
 public class FloorDTO implements SuperDTO {
+    private int floorNo;
+    private String sensorID;
+
+    public FloorDTO() {
+    }
+
+    public FloorDTO(int floorNo) {
+        this.floorNo = floorNo;
+    }
+
+    public int getFloorNo() {
+        return floorNo;
+    }
+
+    public void setFloorNo(int floorNo) {
+        this.floorNo = floorNo;
+    }
+
+    public String getSensorID() {
+        return sensorID;
+    }
+
+    public void setSensorID(String sensorID) {
+        this.sensorID = sensorID;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "FloorDTO{" +
+                "floorNo=" + floorNo +
+                ", sensorID='" + sensorID + '\'' +
+                '}';
+    }
 }
--- FAMS/FAMSServer/src/main/java/com/fantastic4/server/repository/custom/impl/FloorRepositoryImpl.java
+++ FAMS/FAMSServer/src/main/java/com/fantastic4/server/repository/custom/impl/FloorRepositoryImpl.java
@@ -2,12 +2,34 @@ package com.fantastic4.server.repository.custom.impl;
 
 import com.fantastic4.common.dto.FloorDTO;
 import com.fantastic4.server.repository.custom.FloorRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import java.net.URI;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FloorRepositoryImpl implements FloorRepository {
+
+    private final HttpClient client;
+
+    public FloorRepositoryImpl() {
+        client = HttpClient.newHttpClient();
+    }
+
     @Override
     public boolean save(FloorDTO floorDTO) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/createFloor"))
+                .POST(HttpRequest.BodyPublishers.ofString(floorDTO.toString()))
+                .build();
+
+        HttpResponse<?> response = client.send(request, HttpResponse.BodyHandlers.discarding());
+        System.out.println(response.statusCode());
         return false;
     }
 
@@ -28,6 +50,23 @@ public class FloorRepositoryImpl implements FloorRepository {
 
     @Override
     public List<FloorDTO> findAll() throws Exception {
-        return null;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/getAllFloors"))
+                .build();
+
+        List<FloorDTO> floorDTOList = new ArrayList<>();
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONArray jsonArray = new JSONArray(response.body());
+        ObjectMapper mapper = new ObjectMapper();
+        jsonArray.forEach(object -> {
+            try {
+                floorDTOList.add(mapper.readValue(object.toString(), FloorDTO.class));
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return floorDTOList;
     }
 }
